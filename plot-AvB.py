a7c6eab376430c12264bf5261a57a0beabc775e2
AntiJoe <jrallen.automation@gmail.com>
2021-02-10 07:22:30 -0400

    add bar chart

--- plot-AvB.py
+++ plot-AvB.py
@@ -15,7 +15,7 @@ def rr(rej, feed):
 lab = pd.read_csv('labdata.csv')  
 samplepoints = pd.read_csv('sample-points.csv')
 pidata = pd.read_csv('pidata.csv')
-
+calcs = pd.read_csv('screencalcs.csv')
 
 df = lab.join(samplepoints.set_index('sample'), on='sample')
 df['admt'] = admt(df.cst, df.flow)
@@ -31,7 +31,7 @@ fsrrm = pd.DataFrame(np.array([[1,0,0],[2,0,0], [3,0,0]]),
 print(df)
 print(df.describe())
 
-print(fsrrm)
+print(calcs)
 
 
 
@@ -39,3 +39,23 @@ print(fsrrm)
 fig = plt.figure('Screen Audit Feb 3')
 ax = fig.add_subplot(1,1,1)
 
+fig.suptitle('Screen Audit Feb 3, 2021')
+font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
+
+# plt.title('Quality Window\n' + start_time + ' --  ' + end_time, fontdict=font, loc='left')
+# plt.legend(['MC6', 'MC1', 'Rejects', 'L1', 'L2', 'L3'])
+
+font['size'] = 12
+plt.xlabel('Trial', fontdict=font)
+plt.ylabel('Reject Rate (mass)', fontdict=font)
+
+plt.style.use('seaborn-dark-palette')
+# plt.scatter(x=calcs['Feed.cst'], y=calcs.Frrm, s=9, c='black', marker='o')
+ax.bar(calcs['Trial'], calcs.Frrm, width=0.25, bottom=0)
+# plt.ylim(0, 100)
+
+plt.show()
