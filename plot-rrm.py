1691726cc5244610f34e7184de144c639b63f76c
AntiJoe <jrallen.automation@gmail.com>
2021-02-11 18:24:05 -0400

    added RRm combined chart

--- plot-rrm.py
+++ plot-rrm.py
@@ -11,44 +11,42 @@ print(dff)
 
 dff = dff[dff.Trial != 'SD']
 
-for screenTag in ['FS', 'P', 'S']:
+# Create plot figure and axis
+fig = plt.figure('Screen Audit Feb 3')
+ax = fig.add_subplot(1,1,1)
+font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
+font['size'] = 12
+barWidth = 0.15
+fig.suptitle('Reject Rates by Mass vs Feed Consistency\nIndividual and Combined', fontdict=font)
+c = ['b', 'g', 'r']
+offset = [-barWidth, 0, barWidth]
+trial= [3,3]
+trialLegend = ['Trial 1 - 1.3%', 'Trial 2 - 1.55%', 'Trial 3 - 1.0%']
+
+for screenTag in ['FS', 'P', 'S', 'Combined']:
     df = dff[dff.Screen == screenTag]
     print(df)
     print(df.describe())
     
-
-
-    # Create plot figure and axis
-    fig = plt.figure('Screen Audit Feb 3')
-    ax = fig.add_subplot(1,1,1)
-    font = {'family': 'serif',
-            'color':  'darkred',
-            'weight': 'normal',
-            'size': 16,
-            }
-    font['size'] = 12
-    barWidth = 0.15
-    fig.suptitle('{} Screen \nFreeness Streams vs Feed Consistency'.format(screenTag))
-    c = ['b', 'g', 'r']
-    offset = [-barWidth, 0, barWidth]
-    trial= [3,3]
-    trialLegend = ['Trial 1 - 1.3%', 'Trial 2 - 1.55%', 'Trial 3 - 1.0%']
-
     # plt.scatter(x=df.feed, y=df.RRm)
-    ax = sns.regplot(x=df.feed, y=df.RRm, ci=None)
+    ax = sns.regplot(x=df.feed, y=df.RRm, ci=None, label=screenTag)
         
-    # ax.legend.texts[0].set_text([1,2,3])
-    # plt.legend()
-    plt.grid(True, ls=':')
-    
-    plt.xlabel('Feed Consistency (%)', fontdict=font)
-    plt.ylabel('Reject Rate by Mass', fontdict=font)
-    xpad = 0.1
-    ypad = 1
-    plt.xlim(min(df.feed) - xpad, max(df.feed) + xpad)
-    plt.ylim(min(df.RRm) - ypad, max(df.RRm) + ypad)
-    # plt.yticks(ticks=np.arr)
-    outfile = '{}-con-rrm.png'.format(screenTag)
-    plt.savefig(outfile, dpi=600)
-    plt.show()
-    plt.close()
+# ax.legend.texts[0].set_text([1,2,3])
+plt.legend()
+plt.grid(True, ls=':')
+
+plt.xlabel('Feed Consistency (%)', fontdict=font)
+plt.ylabel('Reject Rate by Mass', fontdict=font)
+xpad = 0.1
+ypad = 5
+plt.xlim(min(dff.feed) - xpad, max(dff.feed) + xpad)
+plt.ylim(min(dff.RRm) - ypad, max(dff.RRm) + ypad)
+# plt.yticks(ticks=np.arr)
+# outfile = '{}-con-rrm.png'.format(screenTag)
+# plt.savefig(outfile, dpi=600)
+plt.show()
+plt.close()
